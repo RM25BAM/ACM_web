80b4ea8cabcf7fa28c52704d4e13fb783b41d797
RM25BAM <nmpiedrabuena2003@gmail.com>
2025-02-14 15:30:00 -0500

    acm website updates

---
+++ client/src/pages/home.tsx
@@ -0,0 +1,19 @@
+import Navbar from "../section/navbar"
+
+export const home = () => {
+    return (
+
+        <div className="h-40 ">
+            <Navbar />
+            <div>
+                Hello world
+            </div>
+            <div>
+                Hello
+            </div>
+        </div>
+    )
+}
+export default home
+
+/* thinking of tiles */
