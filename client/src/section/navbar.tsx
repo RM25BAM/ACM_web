997b68c46e41f32839a39bc0928e3aa008c8f108
RM25BAM <nmpiedrabuena2003@gmail.com>
2025-02-22 15:30:00 -0500

    acm website updates

--- client/src/section/navbar.tsx
+++ client/src/section/navbar.tsx
@@ -1,34 +1,46 @@
-import React from "react"
-import { navItems } from "./mapItems"
+import React from "react";
+import { navItems } from "./mapItems";
 import Logo from "../assets/acm_logo.svg";
-const navbar: React.FC = () => {
+import { useNavigate } from "react-router-dom";
+
+const Navbar: React.FC = () => {
+    const navigate = useNavigate();
+
     return (
-        <header className="bg-white shadow-2xl h-16">
-            <div className="flex items-center justify-center h-full px-4">
-                <img className="h-16" src={Logo} alt="logo" />
+        <header className="bg-white shadow-2xl h-18">
+            <div className="flex items-center h-full px-4 w-full justify-between md:justify-center">
+
+                {/* Logo and Chapter */}
+                <div className="flex items-center gap-2">
+                    <img className="h-16" src={Logo} alt="logo" />
+                    <button
+                        className="text-black hover:text-gray-700 focus:outline-none text-md font-bold"
+                        onClick={() => navigate("/")}
+                    >
+                        chapter at MU
+                    </button>
+                </div>
+
+                {/*  (Visible on md and larger) */}
                 <nav className="hidden md:flex gap-x-10 flex-1 justify-center">
                     {navItems.map((item) => (
                         <button
                             key={item.label}
-                            onClick={() => item.navigate}
+                            onClick={() => navigate(item.navigate)}
                             className="text-gray-700 hover:text-blue-600 focus:outline-none text-md font-semibold font-Ubuntu"
                         >
                             {item.label}
                         </button>
                     ))}
                 </nav>
-                <div className="border-2 flex-end hidden md:flex">
-                    <button
-                        onClick={() => '#join'}
-                        className="text-gray-700 hover:text-blue-600 focus:outline-none text-md font-semibold font-Ubuntu"
-                    >
-                        Join Us
-                    </button>
+
+               
+                <div className="hidden md:flex border-2">
+                    {/* You can add elements here if necessary */}
                 </div>
             </div>
         </header>
+    );
+};
 
-    )
-}
-
-export default navbar
+export default Navbar;
