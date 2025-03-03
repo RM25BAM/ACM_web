7bfad4045cc03e1a4d6ece3be2ca57688ad8ef66
RM25BAM <nmpiedrabuena2003@gmail.com>
2025-03-03 10:00:00 -0500

    Backdated commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,25 +1,29 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
-import Preloader from './pages/preloader';
+import Preloader from './pages/preloader'; // Import the Preloader component
 import './App.css';
-
+import CustomCursor from './components/customCursor';
 const App: React.FC = () => {
-  const [loading, setLoading] = useState(true); // Track loading state
+  const [loading, setLoading] = useState(false); // Track loading state
 
   return (
-    <BrowserRouter>
-      {loading ? (
-        <Preloader onComplete={() => setLoading(false)} /> // DEFAULT IS TRUE unless set by Usestate as true
-      ) : (
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      )}
-    </BrowserRouter>
+    <>
+      <CustomCursor />
+      <BrowserRouter>
+        {loading ? (
+          <Preloader onComplete={() => setLoading(false)} /> // Show Preloader first
+        ) : (
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        )}
+      </BrowserRouter>
+    </>
   );
 }
 
 export default App;
+
 
 
