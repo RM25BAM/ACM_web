22f284d6f51136723b736ef84d98839d14f698f8
RM25BAM <nmpiedrabuena2003@gmail.com>
2025-03-08 22:56:21 -0500

    changes

--- client/src/section/navbar.tsx
+++ client/src/section/navbar.tsx
@@ -1,23 +1,36 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { navItems } from "./mapItems";
 import Logo from "../assets/acm_logo.svg";
 import { useNavigate } from "react-router-dom";
+import Portal from "../assets/portal_doom.gif"
 
 const Navbar: React.FC = () => {
     const navigate = useNavigate();
-
+    const [isScrolled, setIsScrolled] = useState(false);
+    const handleScroll = () => {
+        if (window.scrollY > 50) {
+            setIsScrolled(true);
+        }
+        else {
+            setIsScrolled(false);
+        }
+    }
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
     return (
-        <nav className="sticky bg-white shadow-2xl h-18">
+        /* border-b-stone-300 */
+        <nav className={`sticky z-50 top-0 h-18 bg-transparent ${isScrolled}`}>
             <div className="flex items-center h-full px-4 w-full justify-between md:justify-center">
-
-                {/* Logo and Chapter */}
                 <div className="flex items-center gap-2">
-                    <img className="h-16" src={Logo} alt="logo" />
+                    <img className="h-12 w-12" src={Logo} alt="logo" />
                     <button
-                        className="text-black hover:text-gray-700 focus:outline-none text-md font-bold"
+                        className="text-darkG hover:text-gray-700 focus:outline-none text-md font-bold"
                         onClick={() => navigate("/")}
                     >
-                        chapter at MU
                     </button>
                 </div>
 
@@ -27,16 +40,16 @@ const Navbar: React.FC = () => {
                         <button
                             key={item.label}
                             onClick={() => navigate(item.navigate)}
-                            className="text-gray-700 hover:text-blue-600 focus:outline-none text-md font-semibold font-Ubuntu"
+                            className="text-darkG hover:text-blue-600 focus:outline-none text-md font-semibold font-Ubuntu"
                         >
                             {item.label}
                         </button>
                     ))}
                 </div>
 
-               
-                <div className="hidden md:flex border-2">
-                    {/* You can add elements here if necessary */}
+
+                <div className="hidden md:flex">
+                    <img src={Portal} className="h-18 " />
                 </div>
             </div>
         </nav>
